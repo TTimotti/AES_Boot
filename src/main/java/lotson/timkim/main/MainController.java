5266ceb21c2042f377b7de3795209ec21fd77a61
TTimotti <ttimotti@kakao.com>
2024-05-02 15:55:05 +0900

    MainController 수정
    - index 화면 추가

--- src/main/java/lotson/timkim/main/MainController.java
+++ src/main/java/lotson/timkim/main/MainController.java
@@ -1,11 +1,10 @@
 package lotson.timkim.main;
 
-import lotson.timkim.dto.HomeDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
@@ -13,36 +12,7 @@ public class MainController {
     private static final Logger log = LoggerFactory.getLogger(MainController.class);
 
     @GetMapping
-    @Main.EncryptParams
-    public String getHome(Model model, HomeDTO homeDTO) {
-        model.addAttribute("name", homeDTO.getName());
-        model.addAttribute("age", homeDTO.getAge());
-
-        return "index";
+    public String getIndex() {
+        return "encrypt";
     }
-
-    @PostMapping
-    @Main.EncryptParams
-    public String postHome(Model model, HomeDTO homeDTO) {
-        model.addAttribute("name", homeDTO.getName());
-        model.addAttribute("age", homeDTO.getAge());
-
-        return "index";
-    }
-
-    @GetMapping("/api")
-    @ResponseBody
-    @Main.EncryptParams
-    public HomeDTO ajaxGet(Model model, HomeDTO homeDTO) {
-        return homeDTO;
-    }
-
-
-    @PostMapping("/api")
-    @ResponseBody
-    @Main.EncryptParams
-    public HomeDTO ajaxPost(Model model, HomeDTO homeDTO) {
-        return homeDTO;
-    }
-
 }
